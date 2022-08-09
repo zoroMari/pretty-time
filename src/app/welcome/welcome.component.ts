e49a83985a48241f15540f78437b71a10264eb79
Mariia Moroz <mashanya.kov@gmail.com>
2022-08-09 15:24:18 +0300

    added welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TimeService } from '../time.service';
 
 @Component({
@@ -12,11 +12,16 @@ export class WelcomeComponent implements OnInit {
 
   constructor(
     private _route: ActivatedRoute,
+    private _router: Router,
     private _timeService: TimeService,
   ) {}
 
   ngOnInit(): void {
     this._timeService.title.next(this._route.snapshot.data['title']);
+  }
+
+  handleNavigate(route: string) {
+    this._router.navigate([route]);
   }
 
 }
